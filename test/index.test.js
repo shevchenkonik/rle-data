df6af7a6d6b84b5528dec086729992dd6fcdd480
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-11-15 22:36:57 +0300

    Fix typo in test

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,12 @@
 let RLe = require('../')
 
-it('RLE decodes', () => {
+it('RLE encodes', () => {
   let encodedData = RLe.encode([0, 0, 0, 0, 0, 1]);
 
   expect(encodedData).toEqual([5, 0, 1, 1]);
 })
 
-it('RLE encodes', () => {
+it('RLE decodes', () => {
   let decodedData = RLe.decode([5, 0, 1, 1, 5, 0, 1, 0]);
 
   expect(decodedData).toEqual([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]);
