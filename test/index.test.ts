78392ddf1ae5495512578649b2c3bbc6fe17bb1f
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-12-05 13:11:34 +0300

    Testing string value

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,49 @@
 let RLe = require('../src/')
 
-it('RLE encodes', () => {
-  let encodedData = RLe.encode([0, 0, 0, 0, 0, 1, 0]);
+let data = {
+  toEncode: {
+    numbers: [0, 0, 0, 0, 0, 1, 0],
+    strings: ["a", "a", "a", "b"],
+    stringValue: "aaab"
+  },
 
-  expect(encodedData).toEqual([5, 0, 1, 1, 1, 0]);
+  toDecode: {
+    numbers: [5, 0, 1, 1, 1, 0],
+    strings: [3, "a", 1, "b"],
+  }
+};
+
+describe("Encode", () => {
+  it("Numbers", () => {
+    let encodedData = RLe.encode(data.toEncode.numbers);
+
+    expect(encodedData).toEqual(data.toDecode.numbers);
+  })
+
+  describe("Strings", () => {
+    let encodedData = RLe.encode(data.toEncode.strings);
+    let encodedDataString = RLe.encode(data.toEncode.stringValue);
+
+    it("Array of strings", () => {
+      expect(encodedData).toEqual(data.toDecode.strings);
+    })
+
+    it("String value", () => {
+      expect(encodedDataString).toEqual(data.toDecode.strings)
+    })
+  })
 })
 
-it('RLE decodes', () => {
-  let decodedData = RLe.decode([5, 0, 1, 1, 5, 0, 1, 0]);
+describe("Decode", () => {
+  it('Numbers', () => {
+    let decodedData = RLe.decode(data.toDecode.numbers);
 
-  expect(decodedData).toEqual([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]);
+    expect(decodedData).toEqual(data.toEncode.numbers);
+  })
+
+  it('Strings', () => {
+    let decodedData = RLe.decode(data.toDecode.strings);
+
+    expect(decodedData).toEqual(data.toEncode.strings);
+  })
 })
